5728aa7a7be7b46838e96cd4704696d94e34aabe
Laura Iglesias <laura.iglesias.pina@gmail.com>
2015-01-27 21:12:41 +0100

    Initial Commit

---
+++ Fontopaedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fontopaedia
+//
+//  Created by Laura Iglesias Piña on 27/1/15.
+//  Copyright (c) 2015 lip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
